18567e57b5f5f37fc81d9e6f29c3a9bdfda9ed55
TylerGaston22 <78615898+TylerGaston22@users.noreply.github.com>
2026-09-01 19:58:13 +0000

    input practice

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python_Project
+
+This project to to practice coding in Python.
---
+++ main.py
@@ -0,0 +1,66 @@
+# This is my first Python program
+
+# Print practice
+# print("I like pizza!")
+# print("It's really good!")
+
+# Variable practice
+# strings
+# first_name = "Jon"
+# food = "pizza"
+
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+
+# # integer
+# age = 22
+
+# print(f"You are {age} years old")
+
+# # Float
+# price = 10.99
+
+# print(f"the price is {price}")
+
+# #boolean
+# is_student = True
+# for_sale = False
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are not a student")
+
+# if for_sale:
+#     print("That item is for sale")
+# else:
+#     print("That item is not for sale")
+
+
+# Typecasting
+
+# name = "Jon"
+# age = 25
+# gpa = 3.8
+# is_student = True
+
+# print(type(name))
+
+# gpa = int(gpa)
+# print(gpa)
+
+# age = float(age)
+# print(age)
+
+# age = str(age)
+# print(age)
+# print(type(age))
+
+# can use bool to check for user input
+# name = bool(name)
+# print(name)
+
+# # input() = Function practice
+# name = input("What is your name?: ")
+# print(f"Hello {name}.")
+
